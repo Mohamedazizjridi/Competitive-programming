cf53c8f42ed23eebc805baf1b5470b56cc3c1ceb
Mohamedazizjridi <153727546+Mohamedazizjridi@users.noreply.github.com>
2025-10-06 12:03:08 +0100

    Update Shortest-RoutesI.cpp

--- Graph Algorithms/Shortest-RoutesI.cpp
+++ Graph Algorithms/Shortest-RoutesI.cpp
@@ -1,34 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using ld = long double;
 void solve() {
   int n,m;cin>>n>>m;
-  vector<vector<pair<int,int>>> edge(n);
+  vector<vector<pair<ll,ll>>> adj(n);
   for(int i=0;i<m;i++){
-    int u,v;cin>>u>>v;
-    int w;cin>>w;
+    ll u,v;cin>>u>>v;
+    ll w;cin>>w;
     u--;v--;
-    edge[u].push_back({v,w});
-    edge[v].push_back({u,w});
+    adj[u].push_back({v,w});
   }
-  vector<bool>visited(n,false);
-  priority_queue<pair<int,int>>pq;
-  vector<int>dist(n,INT_MAX);
-  pq.push({0,0});
-  dist[0]=0;
-  while(!pq.empty()){
-    auto[w,c]=pq.top();
-    pq.pop();
-    if(visited[c]) continue;
-    visited[c]=true;
-    for(auto [e,x] :  edge[c]){
-      if(dist[c]+x<dist[e]){
-        dist[e]=dist[c]+x;
-        pq.push({-dist[e],e});
-      }
+   vector<ll> distance(n, LLONG_MAX);
+    vector<bool> processed(n ,false);
+    priority_queue<pair<ll, ll>> q;
+    distance[0] = 0;
+    q.push({0, 0});
+    while (!q.empty()) {
+        ll a = q.top().second;
+        q.pop();
+        if (processed[a]) continue;
+        processed[a] = true;
+        for (auto u : adj[a]) {
+            ll b = u.first, w = u.second;
+            if (distance[a] + w < distance[b]) {
+                distance[b] = distance[a] + w;
+                q.push({-distance[b], b});
+            }
+        }
     }
-  }
-  for(auto e : dist){
+  for(auto e : distance){
     cout<<e<<" ";
   }
   cout<<endl;
